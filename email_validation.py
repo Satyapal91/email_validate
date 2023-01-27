118a3bb25bca243ea6d7d2b6c24d3cf1053c9fb7
Satyapal91 <satyapalverma001@gmail.com>
2023-01-27 16:52:17 +0530

    core code of email validation

---
+++ email_validation.py
@@ -0,0 +1,34 @@
+# email validation condition ------  a@b.in -- good@gamil.com
+
+Email = input("Enter the valid email address: ")
+k, l, m = 0, 0, 0
+
+if len(Email) >= 6:
+    if Email[0].isalpha():
+        if ("@" in Email) and (Email.count("@")==1):
+            if (Email[-4] == ".") ^ (Email[-3] == "."):
+                for i in Email:
+                    if i == i.isspace():
+                        k = 1
+                    elif i.isalpha():
+                        if i == i.upper():
+                            l = 1
+                    elif i.isdigit():
+                        continue
+                    elif i == "_" or i == "." or i == "@":
+                        continue
+                    else:
+                        m = 1
+                else:
+                    if k == 1 or l == 1 or m == 1:
+                        print("wrong email selected 5")
+                    else:
+                        print("right email address")
+            else:
+                print("wrong email selected 4")
+        else:
+            print("wrong email selected 3")
+    else:
+        print("wrong email selected 2")
+else:
+    print("wrong email selected 1")
